71a3321b104469e3fbf0f17721ff71e1f52fe364
Tan Sheng Da <shengdatan@gmail.com>
2025-03-19 00:38:26 +0800

    Initialise shadcn/ui

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import path from 'node:path'
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -7,6 +9,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [
     vue(),
     vueJsx(),
@@ -14,7 +21,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
