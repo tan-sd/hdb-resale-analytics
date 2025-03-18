79021737fc6ff480f58c2cf2d31620d50f1ed808
Tan Sheng Da <shengdatan@gmail.com>
2025-03-19 00:39:22 +0800

    Add title and dashboard

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <script setup>
 import BarChart from '../components/BarChart.vue'
 import MapChart from '../components/MapChart.vue'
+import Title from '../components/Title.vue'
+import Dashboard from '@/components/Dashboard.vue'
 </script>
 
 <template>
   <main>
     <!-- <BarChart /> -->
+     <Title />
+    <Dashboard />
     <MapChart />
   </main>
 </template>
