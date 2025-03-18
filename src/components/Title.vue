d22cc7df2f34d6814f580cfd9f561209b22cf0bf
Tan Sheng Da <shengdatan@gmail.com>
2025-03-19 00:39:08 +0800

    Add title for project

---
+++ src/components/Title.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="title">
+        <div id="main-title">Behind the Price Tag</div>
+        <div id="sub-title">A deep dive into Singapore's HDB Resale Market</div>
+    </div>
+</template>
