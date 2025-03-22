0e59c59070eb7f0f1478464ef9e63f05f7ef7d24
tan-sd <shengdatan@gmail.com>
2025-03-23 00:07:31 +0800

    Insert title

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="title">
-        <div id="main-title">Behind the Price Tag</div>
+        <div id="main-title" class="font-bold">Behind the Price Tag</div>
         <div id="sub-title">A deep dive into Singapore's HDB Resale Market</div>
     </div>
 </template>
