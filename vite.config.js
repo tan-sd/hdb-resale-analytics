2f5761a3238282abd7e0f07858949a811ae5f226
tan-sd <shengdatan@gmail.com>
2025-03-23 00:05:33 +0800

    Initialise shadcn

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
-import path from 'node:path'
-import autoprefixer from 'autoprefixer'
-import tailwind from 'tailwindcss'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from "node:url";
+import path from "node:path";
+import autoprefixer from "autoprefixer";
+import tailwind from "tailwindcss";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwind(), autoprefixer()],
+    css: {
+        postcss: {
+            plugins: [tailwind(), autoprefixer()],
+        },
     },
-  },
-  plugins: [
-    vue(),
-    vueJsx(),
-    // vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [
+        vue(),
+        vueJsx(),
+        // vueDevTools(),
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-})
+    define: {
+        "process.env": {},
+    },
+});
