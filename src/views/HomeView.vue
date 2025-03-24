bfac200d934f0f82a36a759fa2f4906d45c3014f
tan-sd <shengdatan@gmail.com>
2025-03-24 22:49:43 +0800

    Add section1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,14 +3,14 @@ import BarChart from '../components/barChart.vue'
 import MapChart from '../components/MapChart.vue'
 import Title from '../components/Title.vue'
 import Dashboard from '@/components/Dashboard.vue'
-import LineChart from '../components/LineChart.vue'
+import Section1 from '../components/Section1.vue'
 </script>
 
 <template>
   <main>
     <!-- <BarChart /> -->
      <Title />
-    <LineChart />
+    <Section1 />
     <Dashboard />
     <!-- <MapChart /> -->
   </main>
