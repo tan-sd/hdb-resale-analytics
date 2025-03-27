dc6d0668187a826f22ece726c287632856b0243e
tan-sd <shengdatan@gmail.com>
2025-03-27 12:48:42 +0800

    Add Section2 with factors

---
+++ src/components/Section2.vue
@@ -0,0 +1,94 @@
+<template>
+    <section class="w-screen min-h-screen px-8 py-20 bg-white">
+        <div
+            class="max-w-7xl mx-auto flex flex-col lg:flex-row items-start justify-between gap-12"
+        >
+            <div class="w-full lg:w-2/5 space-y-6">
+                <div>
+                    <h2
+                        class="text-3xl md:text-5xl font-bold mb-4 uppercase tracking-tight section-title"
+                    >
+                        Factors
+                    </h2>
+                    <div>
+                        <div
+                            v-for="step in steps"
+                            :key="step.title"
+                            :data-title="step.title"
+                            class="min-h-[80vh] flex flex-col justify-content space-y-2"
+                        >
+                            <p
+                                class="uppercase text-sm text-muted-foreground font-semibold tracking-wide"
+                            >
+                                {{ step.title }}
+                            </p>
+
+                            <div
+                                class="space-y-5 text-sm text-muted-foreground leading-relaxed"
+                            >
+                                <p
+                                    v-for="(para, index) in step.description"
+                                    :key="index"
+                                >
+                                    {{ para }}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-full lg:w-3/5 lg:sticky lg:top-32">
+                <!-- <LineChart
+                    :highlightYears="activeYears"
+                    :shouldHighlight="activeHighlight"
+                /> -->
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup>
+const steps = [
+    {
+        title: "Stories",
+        description: [
+            "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
+        ],
+        years: [
+            1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000,
+            2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011,
+            2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022,
+            2023,
+        ],
+    },
+    {
+        title: "Flat types and sizes",
+        description: [
+            "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
+        ],
+        years: [1990, 1991, 1992, 1993, 1994],
+    },
+    {
+        title: "Price per square metre HDB in different planning areas",
+        description: [
+            "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
+        ],
+        years: [1994, 1995, 1996],
+    },
+    {
+        title: "Number of amenities and distance to amenities",
+        description: [
+            "The number of amenities and distance to amenities are important factors that influence the resale price of HDB flats.",
+        ],
+        years: [1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006],
+    },
+    {
+        title: "Electoral boundaries",
+        description: [
+            "Grant enhancements and limited BTO supply pushed resale prices higher.",
+        ],
+        years: [2007, 2008, 2009, 2010, 2011, 2012, 2013],
+    },
+];
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,17 @@
 <script setup>
-import BarChart from '../components/barChart.vue'
-import MapChart from '../components/MapChart.vue'
-import Title from '../components/Title.vue'
-import Dashboard from '@/components/Dashboard.vue'
-import Section1 from '../components/Section1.vue'
+import BarChart from "../components/barChart.vue";
+import MapChart from "../components/MapChart.vue";
+import Title from "../components/Title.vue";
+import Dashboard from "@/components/Dashboard.vue";
+import Section1 from "../components/Section1.vue";
+import Section2 from "../components/Section2.vue";
 </script>
 
 <template>
-  <main>
-    <!-- <BarChart /> -->
-     <Title />
-    <Section1 />
-    <Dashboard />
-    <!-- <MapChart /> -->
-  </main>
+    <main>
+        <Title />
+        <Section1 />
+        <Section2 />
+        <Dashboard />
+    </main>
 </template>
