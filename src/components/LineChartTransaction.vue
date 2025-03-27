357f603c29aebe9534504dce50388bd735a4188e
tan-sd <shengdatan@gmail.com>
2025-03-27 12:47:53 +0800

    Initialise LineChartTransaction

---
+++ src/components/LineChartTransaction.vue
@@ -0,0 +1,312 @@
+<template>
+    <div
+        class="chart-container relative flex flex-col items-center justify-center mt-3"
+    >
+        <h3 class="text-sm mb-2 text-center">
+            Total HDB Transactions per Year
+        </h3>
+        <div id="line-chart">
+            <svg
+                viewbox="0 0 1000 600"
+                :width="width + margin.left + margin.right"
+                :height="height + margin.top + margin.bottom"
+            >
+                <g
+                    :transform="
+                        'translate(' + margin.left + ',' + margin.top + ')'
+                    "
+                />
+            </svg>
+        </div>
+        <div
+            id="tooltip-transaction"
+            style="
+                position: absolute;
+                padding: 8px;
+                background: rgba(0, 0, 0, 0.7);
+                color: white;
+                border-radius: 5px;
+                display: none;
+            "
+        ></div>
+    </div>
+</template>
+
+<script>
+import * as d3 from "d3";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+import { watch } from "vue";
+
+export default {
+    name: "LineChart",
+    props: {
+        highlightYears: {
+            type: Array,
+            default: () => [],
+        },
+        shouldHighlight: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    data() {
+        return {
+            margin: { top: 20, right: 30, bottom: 40, left: 60 },
+            width: 0,
+            height: 0,
+            dotSelection: null,
+        };
+    },
+    mounted() {
+        this.setDimensions();
+
+        window.addEventListener("resize", this.setDimensions);
+
+        Promise.all([
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_1990.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_1991.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_1992.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_1993.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_1994.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_1995.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_1996.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_1997.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_1998.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_1999.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2000.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2001.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2002.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2003.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2004.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2005.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2006.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2007.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2008.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2009.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2010.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2011.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2012.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2013.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2014.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2015.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2016.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2017.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2018.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2019.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2020.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2021.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2022.csv"),
+            d3.csv("data/resale_prices_cleaned/HDBPriceWithSubzone_2023.csv"),
+        ]).then(this.createChart);
+    },
+    methods: {
+        setDimensions() {
+            const container = this.$el.querySelector("#line-chart");
+            if (container) {
+                this.width =
+                    container.clientWidth -
+                    this.margin.left -
+                    this.margin.right;
+                this.height =
+                    container.clientHeight -
+                    this.margin.top -
+                    this.margin.bottom;
+            }
+        },
+
+        createChart(data) {
+            const allData = [].concat(...data);
+
+            allData.forEach((d) => {
+                d.Year = +d.Year;
+                d.Month = +d.Month;
+                d.ResalePrice = +d["Resale Price"];
+                d.Date = new Date(d.Year, d.Month - 1);
+            });
+
+            const groupedByYear = d3.group(allData, (d) => d.Year);
+
+            const yearCounts = Array.from(groupedByYear, ([year, values]) => {
+                return {
+                    Year: year,
+                    count: values.length,
+                };
+            });
+
+            const x = d3
+                .scaleTime()
+                .domain(d3.extent(yearCounts, (d) => new Date(d.Year, 0)))
+                .range([0, this.width]);
+
+            const y = d3
+                .scaleLinear()
+                .domain([0, d3.max(yearCounts, (d) => d.count)])
+                .range([this.height, 0]);
+
+            const highlightRange = () => {
+                svg.selectAll(".highlight-rect").remove();
+
+                if (this.shouldHighlight && this.highlightYears.length < 34) {
+                    const minYear = d3.min(this.highlightYears);
+                    const maxYear = d3.max(this.highlightYears);
+
+                    svg.insert("rect", ":first-child")
+                        .attr("class", "highlight-rect")
+                        .attr("x", x(new Date(minYear, 0)))
+                        .attr("y", 0)
+                        .attr(
+                            "width",
+                            x(new Date(maxYear, 0)) - x(new Date(minYear, 0))
+                        )
+                        .attr("height", this.height)
+                        .attr("fill", "#fef3c7")
+                        .attr("opacity", 0.4);
+                }
+            };
+
+            const svg = d3.select(this.$el).select("svg").select("g");
+
+            svg.append("g")
+                .attr("transform", `translate(0,${this.height})`)
+                .call(d3.axisBottom(x));
+
+            svg.append("g").call(d3.axisLeft(y));
+
+            const line = d3
+                .line()
+                .x((d) => x(new Date(d.Year, 0)))
+                .y((d) => y(d.count));
+
+            const path = svg
+                .append("path")
+                .data([yearCounts])
+                .attr("class", "line")
+                .attr("d", line)
+                .style("fill", "none")
+                .style("stroke", "steelblue")
+                .style("stroke-width", 2);
+
+            const pathElement = path.node();
+            const totalLength = pathElement.getTotalLength();
+
+            path.attr("stroke-dasharray", `${totalLength} ${totalLength}`).attr(
+                "stroke-dashoffset",
+                totalLength
+            );
+
+            const tooltip = d3.select("#tooltip-transaction");
+
+            this.dotSelection = svg
+                .selectAll(".dot")
+                .data(yearCounts)
+                .enter()
+                .append("circle")
+                .attr("class", "dot")
+                .attr("r", 4)
+                .attr("cx", (d) => x(new Date(d.Year, 0)))
+                .attr("cy", (d) => y(d.count))
+                .style("fill", "red")
+                .style("opacity", 0)
+                .on("mouseover", (event, d) => {
+                    tooltip
+                        .style("display", "inline-block")
+                        .html(
+                            `Year: ${
+                                d.Year
+                            }<br>Transactions: ${new Intl.NumberFormat().format(
+                                d.count
+                            )}`
+                        );
+                    d3.select(event.target).attr("r", 6);
+                })
+                .on("mousemove", function (event, d) {
+                    const [mouseX, mouseY] = d3.pointer(event);
+
+                    tooltip
+                        .style("top", `${mouseY - 30}px`)
+                        .style("left", `${mouseX + 10}px`);
+                })
+                .on("mouseout", (event, d) => {
+                    tooltip.style("display", "none");
+
+                    const isHighlighted =
+                        this.shouldHighlight &&
+                        this.highlightYears.includes(d.Year);
+                    d3.select(event.target).attr("r", isHighlighted ? 5 : 4);
+                });
+
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: "#chart-container",
+                    start: "top 80%",
+                    toggleActions: "play none none none",
+                },
+            });
+
+            tl.to(pathElement, {
+                strokeDashoffset: 0,
+                duration: 3,
+                ease: "power2.out",
+            });
+
+            watch(
+                () => [this.highlightYears, this.shouldHighlight],
+                ([newYears, highlight]) => {
+                    if (!this.dotSelection) return;
+
+                    highlightRange();
+
+                    this.dotSelection
+                        .transition()
+                        .duration(300)
+                        .style("opacity", (d) =>
+                            highlight
+                                ? newYears.includes(d.Year)
+                                    ? 1
+                                    : 0.4
+                                : 1
+                        )
+                        .attr("r", (d) =>
+                            highlight ? (newYears.includes(d.Year) ? 5 : 3) : 4
+                        );
+                },
+                { immediate: true, deep: true }
+            );
+        },
+    },
+};
+</script>
+
+<style scoped>
+.chart-container {
+    position: relative;
+    width: 100%;
+    max-width: 800px;
+    height: 300px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+#line-chart {
+    flex: 1;
+    width: 100%;
+    height: 100%;
+    position: relative;
+}
+
+.line {
+    stroke: steelblue;
+    stroke-width: 2;
+}
+
+.dot {
+    fill: red;
+}
+
+.dot:hover {
+    fill: orange;
+}
+</style>
