e662cef54ea43eb7b2f0acbbf8d663c48ca80285
Tan Sheng Da <shengdatan@gmail.com>
2025-03-29 13:19:50 +0800

    Add FactorsOverview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@ import MapChart from "../components/MapChart.vue";
 import Title from "../components/Title.vue";
 import Dashboard from "@/components/Dashboard.vue";
 import Section1 from "../components/Section1.vue";
+import FactorsOverview from "../components/FactorsOverview.vue";
 import Section2 from "../components/Section2.vue";
 </script>
 
@@ -11,6 +12,7 @@ import Section2 from "../components/Section2.vue";
     <main>
         <Title />
         <Section1 />
+        <FactorsOverview />
         <Section2 />
         <Dashboard />
     </main>
