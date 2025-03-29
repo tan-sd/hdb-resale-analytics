659d81722955b93d2b604a0b93915df9015a669d
Tan Sheng Da <shengdatan@gmail.com>
2025-03-30 00:08:37 +0800

    Add styling

--- src/components/GlobalLoader.vue
+++ src/components/GlobalLoader.vue
@@ -1,14 +1,13 @@
 <template>
     <div class="loading-screen" v-if="!isLoadingComplete">
         <div class="loading-content">
-        <h2>Preparing Your Data Visualization</h2>
+          <p class="text-base font-extrabold section-title">{{ loadingStatus }}</p>
         <div class="progress-container">
             <div 
             class="progress-bar" 
             :style="{ width: loadingProgress + '%' }"
             ></div>
         </div>
-        <p>{{ loadingStatus }}</p>
         </div>
     </div>
     <div v-else>
@@ -25,7 +24,7 @@
       const dataStore = useDataStore();
       const isLoadingComplete = ref(false);
       const loadingProgress = ref(0);
-      const loadingStatus = ref('Initializing...');
+      const loadingStatus = ref('Loading Experience...');
   
       const preRenderCharts = [
         {
@@ -45,7 +44,6 @@
             renderMethod: async () => {
             const chartContainer = document.createElement('div');
             chartContainer.style.display = "none";
-            // chartContainer.id = 'transactions-chart-prerender';
             document.body.appendChild(chartContainer);
         
             const { default: LineChartMedian } = await import('@/components/LineChartMedian.vue');
@@ -89,26 +87,21 @@
                 app.mount(chartContainer);
             }
         }
-        // Add more chart pre-rendering methods
       ];
   
       const preloadAllData = async () => {
         try {
-          // Load all data from dataStore
           await dataStore.loadData();
   
-          // Pre-render charts sequentially
           for (let [index, chart] of preRenderCharts.entries()) {
             loadingStatus.value = `Preparing ${chart.name}...`;
             await chart.renderMethod();
             
-            // Update progress
             loadingProgress.value = Math.round(
               ((index + 1) / preRenderCharts.length) * 100
             );
           }
   
-          // Final setup
           isLoadingComplete.value = true;
           emit('loading-complete');
         } catch (error) {
@@ -135,7 +128,7 @@
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: #f0f4f8;
+    background-color: rgb(242, 242, 242) / 1;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -148,7 +141,7 @@
   }
   
   .progress-container {
-    width: 100%;
+    width: 300px;
     background-color: #e0e8f0;
     border-radius: 10px;
     margin: 20px 0;
@@ -157,7 +150,7 @@
   
   .progress-bar {
     height: 10px;
-    background-color: #2c7be5;
+    background-color: hsl(353 75% 53%);
     transition: width 0.5s ease;
   }
   </style>
