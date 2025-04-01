3a73487a6b1339fb8cf149747dd0761fa3e15c09
Tan Sheng Da <shengdatan@gmail.com>
2025-04-01 23:54:21 +0800

    Add new section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,16 +3,18 @@ import MapChart from "../components/MapChart.vue";
 import Title from "../components/Title.vue";
 import Dashboard from "@/components/Dashboard.vue";
 import Section1 from "../components/Section1.vue";
-import FactorsOverview from "../components/FactorsOverview.vue";
 import Section2 from "../components/Section2.vue";
+import FactorsOverview from "../components/FactorsOverview.vue";
+import Section3 from "../components/Section3.vue";
 </script>
 
 <template>
     <main>
         <Title />
         <Section1 />
-        <FactorsOverview />
         <Section2 />
+        <FactorsOverview />
+        <Section3 />
         <Dashboard />
     </main>
 </template>
