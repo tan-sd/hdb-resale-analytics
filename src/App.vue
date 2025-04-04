d999dacb27a56a88ee869a0055964100fc60685e
tan-sd <shengdatan@gmail.com>
2025-04-05 01:59:31 +0800

    Remove unnecessary imports

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { onMounted } from 'vue'
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 import { useDataStore } from './stores/dataStore';
 import GlobalLoader from './components/GlobalLoader.vue';
 
